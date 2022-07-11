c16dbd3397433bea0f9c28ee92d9640b1b0181de
JongHun <minn4072@gmail.com>
2022-07-11 23:40:07 +0900

    beakjoon

--- Bronze/4153직각삼각형/app.js
+++ Bronze/4153직각삼각형/app.js
@@ -4,23 +4,32 @@ const fs = require("fs");
 //input을 읽어와 변수로 선언 & 할당
 // 그 내용을 input에 저장, toString(), split()을 사용해서 Array로 저장
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("/n");
 
 // input 가공부분
-input = input
+let input = fs
+  .readFileSync(filePath)
   .toString()
-  .split(" ")
-  .map((v) => Number(v));
+  .trim()
+  .split("\n")
+  .map((i) => i.split(" ").map(Number));
 
-// console.log(input); // input 값 확인용
+// console.log(input);
 
-let output = sloution(); // solution에 파라미터 넣어주기
-console.log(output); //출력값
+for (let i = 0; i < input.length - 1; i++) {
+  let output = sloution(input[i]); // solution에 파라미터 넣어주기
+  console.log(output); //출력값
+}
 
 ///////////////////////////////////////////////////////////////
 // 문제 로직 구현
 
-function sloution() {
-  let answer;
+function sloution(arr) {
+  let answer = "";
+
+  arr.sort((a, b) => a - b);
+
+  if (arr[0] ** 2 + arr[1] ** 2 === arr[2] ** 2) answer = "right";
+  else answer = "wrong";
+
   return answer;
 }
