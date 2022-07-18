f749d0ccdab6a674783fe26e2f0e654c05363fd1
JongHun <minn4072@gmail.com>
2022-07-18 15:22:29 +0900

    beakjoon

--- Bronze/1259팰린드롬수/app.js
+++ Bronze/1259팰린드롬수/app.js
@@ -4,23 +4,22 @@ const fs = require("fs");
 //input을 읽어와 변수로 선언 & 할당
 // 그 내용을 input에 저장, toString(), split()을 사용해서 Array로 저장
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-// input 가공부분
-input = input
-  .toString()
-  .split(" ")
-  .map((v) => Number(v));
+input = input.map(Number).map(String);
 
-// console.log(input); // input 값 확인용
-
-let output = sloution(); // solution에 파라미터 넣어주기
-console.log(output); //출력값
+sloution(input); // solution에 파라미터 넣어주기
 
 ///////////////////////////////////////////////////////////////
 // 문제 로직 구현
 
-function sloution() {
+function sloution(arr) {
   let answer;
-  return answer;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    answer = arr[i] === arr[i].split("").reverse().join("") ? "yes" : "no";
+    console.log(answer);
+  }
+
+  return;
 }
--- Bronze/15829Hashing/app.js
+++ Bronze/15829Hashing/app.js
@@ -4,23 +4,29 @@ const fs = require("fs");
 //input을 읽어와 변수로 선언 & 할당
 // 그 내용을 input에 저장, toString(), split()을 사용해서 Array로 저장
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
-
-// input 가공부분
-input = input
-  .toString()
-  .split(" ")
-  .map((v) => Number(v));
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 // console.log(input); // input 값 확인용
 
-let output = sloution(); // solution에 파라미터 넣어주기
+let output = sloution(input[1]); // solution에 파라미터 넣어주기
 console.log(output); //출력값
 
 ///////////////////////////////////////////////////////////////
 // 문제 로직 구현
 
-function sloution() {
-  let answer;
+function sloution(str) {
+  let answer = 0;
+  let r = 1;
+  let M = 1234567891;
+
+  [...str].forEach((v) => {
+    result = (v.charCodeAt(0) - 96) * r;
+    r *= 31;
+    r %= M;
+    answer += result;
+  });
+
+  answer %= M;
+
   return answer;
 }
