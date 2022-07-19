3db8acbf560b1ebd48800091af0103499b66c924
JongHun <minn4072@gmail.com>
2022-07-19 19:33:26 +0900

    beakjoon

--- Bronze/2609최대공약수와_최소공배수/app.js
+++ Bronze/2609최대공약수와_최소공배수/app.js
@@ -4,23 +4,28 @@ const fs = require("fs");
 //input을 읽어와 변수로 선언 & 할당
 // 그 내용을 input에 저장, toString(), split()을 사용해서 Array로 저장
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
-
-// input 가공부분
-input = input
-  .toString()
-  .split(" ")
-  .map((v) => Number(v));
+let input = fs.readFileSync(filePath).toString().trim().split(" ");
+input = input.map(Number);
 
 // console.log(input); // input 값 확인용
 
-let output = sloution(); // solution에 파라미터 넣어주기
-console.log(output); //출력값
+sloution(input[0], input[1]); // solution에 파라미터 넣어주기
 
 ///////////////////////////////////////////////////////////////
 // 문제 로직 구현
 
-function sloution() {
+function sloution(a, b) {
   let answer;
-  return answer;
+
+  let gcd = GCD(a, b);
+  let lcd = gcd * (a / gcd) * (b / gcd);
+  console.log(gcd);
+  console.log(lcd);
+  return;
+}
+
+function GCD(a, b) {
+  if (a < b) [a, b] = [b, a];
+  if (b === 0) return a;
+  return GCD(a % b, b);
 }
