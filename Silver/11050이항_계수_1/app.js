1113a3cfb4b01f2142dfa29e0add362e142bc87b
JongHun <minn4072@gmail.com>
2022-07-23 23:02:05 +0900

    beakjoon

--- Silver/11050이항_계수_1/app.js
+++ Silver/11050이항_계수_1/app.js
@@ -1,6 +1,5 @@
 // 파일을 읽어오기 위해 Node.js의 built-in file system module fs 사용
 const fs = require("fs");
-const { DefaultSerializer } = require("v8");
 
 //input을 읽어와 변수로 선언 & 할당
 // 그 내용을 input에 저장, toString(), split()을 사용해서 Array로 저장
