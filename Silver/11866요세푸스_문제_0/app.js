d85488952665153eeeee0487b4bc410824d869a5
JongHun <minn4072@gmail.com>
2022-07-27 23:17:58 +0900

    beakjoon

--- Silver/11866요세푸스_문제_0/app.js
+++ Silver/11866요세푸스_문제_0/app.js
@@ -1,3 +1,43 @@
+class Queue {
+  constructor() {
+    this.storage = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  size() {
+    if (this.storage[this.rear] === undefined) {
+      return 0;
+    } else {
+      return this.rear - this.rear + 1;
+    }
+  }
+
+  add(value) {
+    if (this.size() === 0) {
+      this.storage["0"] = value;
+    } else {
+      this.rear += 1;
+      this.storage[this.rear] = value;
+    }
+  }
+
+  popleft() {
+    let temp;
+    if (this.front === this.rear) {
+      temp = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front = 0;
+      this.rear = 0;
+    } else {
+      temp = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front += 1;
+    }
+    return temp;
+  }
+}
+
 // 파일을 읽어오기 위해 Node.js의 built-in file system module fs 사용
 const fs = require("fs");
 
@@ -5,20 +45,19 @@ const fs = require("fs");
 // 그 내용을 input에 저장, toString(), split()을 사용해서 Array로 저장
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let [N, K] = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
-let arr = [];
+let queue = new Queue();
 let answer = "";
-let count = 0;
 
 for (let i = 1; i <= N; i++) {
-  arr.push(i);
+  queue.add(i);
 }
 
-console.log(arr);
-
-while (arr.length > 0) {
-  count = (count + (K - 1)) % (arr.length + 1);
-  answer += `${arr[count]}, `;
-  arr.splice(count, 1);
+while (queue.size() > 0) {
+  for (let i = 1; i <= K; i++) {
+    if (i === K) answer += `${queue.popleft()}, `;
+    else queue.add(queue.popleft());
+  }
 }
+answer = `<${answer}\b\b>`;
 
 console.log(answer);
