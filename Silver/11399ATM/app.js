697dec44a886a19dce34808fc4215976a31b19cf
JongHun <minn4072@gmail.com>
2022-08-25 23:34:07 +0900

    beakjoon

---
+++ Silver/11399ATM/app.js
@@ -0,0 +1,19 @@
+// 파일을 읽어오기 위해 Node.js의 built-in file system module fs 사용
+const fs = require("fs");
+
+//input을 읽어와 변수로 선언 & 할당
+// 그 내용을 input에 저장, toString(), split()을 사용해서 Array로 저장
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let [n, input] = fs.readFileSync(filePath).toString().trim().split("\n");
+let result = 0;
+
+input = input.split(" ").map(Number);
+
+input.sort((a, b) => a - b);
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j <= i; j++) {
+    result += input[j];
+  }
+}
+console.log(result);
