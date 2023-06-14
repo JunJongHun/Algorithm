bc0f967fa0ebc7bb4c9c8fe65057044959df245a
Jonghun <minn4072@gmail.com>
2023-06-14 16:36:07 +0900

    [level 1] Title: 체육복, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,33 +1,29 @@
+// 학생들의 체육복 상태를 업데이트한다. (여유 옷 가져온사람, 도난당한 사람 포함)
+// 여유 옷을 가진 학생은 앞에 먼저 물어보고 뒤에 물어봐서 빌려준다
+
 function solution(n, lost, reserve) {
     let answer = 0;
-    const arr = Array(n+2).fill(1);
+    // 0 없음, 1 하나, 2 두개
+    const studentState = new Array(n+2).fill(1);  
     
-    for(let i of reserve){
-        arr[i]=2;
-    }
+    for(const n of reserve) studentState[n] += 1;
     
-    for(let i of lost){
-        arr[i]===2 ? arr[i]=1:arr[i]=0;
-    }
+    for(const n of lost) studentState[n] -= 1;
     
-
-    
-    for(let i=1;i<arr.length-1;i++){
-        if(arr[i]===2){
-            if(arr[i-1]===0){
-                arr[i-1]=1;
-                arr[i]=1;
+    for(let i=1; i<=n; i++){
+        if(studentState[i]===2){
+            if(studentState[i-1]===0){
+                studentState[i] -=1;
+                studentState[i-1] +=1;
+                continue;
             }
-            else if(arr[i+1]===0){
-                arr[i+1]=1;
-                arr[i]=1;
+            if(studentState[i+1]===0){
+                studentState[i] -=1;
+                studentState[i+1] +=1;
+                continue;
             }
         }
-        
     }
     
-
-  
-    
-    return arr.filter(x=>x===1||x===2).length-2;
+    return studentState.filter(state => state >= 1).length-2;
 }
