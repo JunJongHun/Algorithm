fb6644a5cf90d47ab147a0c6b970b523b22d817e
Jonghun <minn4072@gmail.com>
2023-06-17 03:30:05 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,20 +1,22 @@
+// 앞에서부터 숫자를 스택에 push
+// push 하려는 값이 스택 마지막 값보다 큰지 비교한다.
+// 넣으려는 값이 크면 스택에서 pop을 한다.
+// 위 과정 반복
+
 function solution(number, k) {
-  let stack = [];
-  let count = 0;
-
-  for (let i = 0; i < number.length; i++) {
-    stack.push(+number[i]);
-
-    while (count < k && stack[stack.length - 1] < number[i + 1]) {
-      stack.pop();
-      count++;
+    const stack = [];
+    let cnt = k;
+    
+    for(const n of number){
+        while(cnt > 0 && stack[stack.length-1] < n){
+            stack.pop();
+            cnt-=1;
+        }
+        stack.push(n);
     }
-  }
+    
+    for(let i=0; i<cnt; i++) stack.pop();
+    
+    return stack.join('');
 
-  while (count < k) {
-    stack.pop();
-    count++;
-  }
-
-  return stack.join("");
 }
