5e46e3f0d25eb320e58b9d04a20975d62060e4c6
Jonghun <minn4072@gmail.com>
2023-08-04 14:47:08 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,22 @@
+function solution(progresses, speeds) {
+  var answer = [];
+  let queue = [];
+  let count = 0;
+  let day = 0;
+
+  while (progresses.length > 0) {
+    if (progresses[0] + day * speeds[0] >= 100) {
+      progresses.shift();
+      speeds.shift();
+      count++;
+    } else {
+      if (count > 0) {
+        answer.push(count);
+        count = 0;
+      }
+      day++;
+    }
+  }
+  answer.push(count);
+  return answer;
+}
