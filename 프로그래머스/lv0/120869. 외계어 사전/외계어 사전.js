618559b912f7ce03621e7307bba51c9cd45ce14c
Jonghun <minn4072@gmail.com>
2023-08-17 14:33:16 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,11 +1,7 @@
 function solution(spell, dic) {
-     
+    
+    const sortedSpell = [...spell].sort().join('');
 
-    for(const d of dic){
-      for(let i =0; i<spell.length; i++){
-          if(!d.includes(spell[i])) break;
-          if(i===spell.length-1) return 1;
-      }
-    }
-    return 2;
+    return dic.some(d => sortedSpell === [...d].sort().join('')) ? 1:2;
 }
+
