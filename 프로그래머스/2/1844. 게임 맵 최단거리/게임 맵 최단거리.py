41aa61213f6d259303489f5bed42d560738c101b
Jonghoon <minn4072@gmail.com>
2024-02-05 07:26:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.19 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,35 +1,31 @@
 from collections import deque
 
 def solution(maps):
-
-    row = len(maps)
-    col = len(maps[0])
-    visited = [[False]*col for _ in range(row)]
-    distance = [[0]*col for _ in range(row)]
     
-    queue =deque()
-    queue.append((0,0))
+    col = len(maps)
+    row = len(maps[0])
+    visited = [[False]*row for _ in range(col)]
+    
+    dy = [-1,1,0,0]
+    dx = [0,0,-1,1]
+    
+    queue = deque()
+    queue.append([0,0,1])
     visited[0][0] = True
-    distance[0][0] = 1
-    
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
     
     while queue:
-        curX,curY = queue.popleft()
+        cy,cx,distance = queue.popleft()
+        
+        if cy == col-1 and cx == row -1:
+            return distance
         
         for i in range(4):
-            nextX = curX + dx[i]
-            nextY = curY + dy[i]
+            ny = cy+dy[i]
+            nx = cx+dx[i]
             
-            if 0<= nextX < row and 0<= nextY < col:
-                if visited[nextX][nextY] == False and maps[nextX][nextY] == 1:
-                    queue.append((nextX,nextY))
-                    visited[nextX][nextY] = True
-                    distance[nextX][nextY] = distance[curX][curY] + 1
-        
-    if distance[row-1][col-1] == 0:
-        return -1
-    
-    
-    return distance[row-1][col-1]
+            if 0<= ny < col and 0<= nx < row:
+                if visited[ny][nx] == False and maps[ny][nx] == 1:
+                    queue.append([ny,nx,distance+1])
+                    visited[ny][nx]=True
+ 
+    return -1
