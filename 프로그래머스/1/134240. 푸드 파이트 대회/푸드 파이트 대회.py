47684ccc21a123175f16e4b429c346f3993961d3
Jonghoon <minn4072@gmail.com>
2024-02-10 14:42:35 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def solution(food):
+    answer = "0"
+    
+    for i in range(len(food)-1,0,-1):
+        
+        for j in range(food[i]//2):
+            answer = str(i)+answer+str(i)
+    
+    return answer
