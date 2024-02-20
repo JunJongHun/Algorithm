487035901b10c5180bf1a9a31bacaf21e6a3fb04
Jonghoon <minn4072@gmail.com>
2024-02-20 14:43:42 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,9 +1,3 @@
--- 코드를 입력하세요
-SELECT
-    ANIMAL_ID,
-    NAME,
-    SEX_UPON_INTAKE
-FROM ANIMAL_INS
-WHERE NAME IN  ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
-
-    
+select animal_id,name,sex_upon_intake
+from animal_ins
+where name in ( 'Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
