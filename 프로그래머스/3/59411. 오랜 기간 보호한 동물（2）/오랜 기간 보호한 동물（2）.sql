1f5ca568a45ccda12218b97de721e0b435388f2a
Jonghoon <minn4072@gmail.com>
2024-02-28 02:59:29 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,6 +1,7 @@
-select a.ANIMAL_ID,a.name
+-- 코드를 입력하세요
+SELECT ANIMAL_ID,a.NAME
 from animal_ins as a
 join animal_outs as b
-on a.ANIMAL_ID = b.ANIMAL_ID
-order by b.datetime - a.datetime desc
+using (animal_id)
+order by datediff(b.DATETIME,a.DATETIME) desc
 limit 2
