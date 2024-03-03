280a797c7ecbecdb5379d3a489002c1f8f644d2f
Jonghoon <minn4072@gmail.com>
2024-03-03 16:29:01 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,4 +1,4 @@
-select ID,	EMAIL,	FIRST_NAME,	LAST_NAME
-from DEVELOPER_INFOS
-where skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python'
-order by id asc
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPER_INFOS
+WHERE 'Python' IN (skill_1, skill_2, skill_3)
+ORDER BY ID ASC
