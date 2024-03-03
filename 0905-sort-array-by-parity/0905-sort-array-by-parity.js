6df3dcec14d77579ece4b12296fb6c1040edea46
Jonghoon <minn4072@gmail.com>
2024-03-04 03:43:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -3,13 +3,21 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    const even = []
-    const odd = []
 
-    for(const i of nums){
-        if(i%2 === 0) even.push(i)
-        else odd.push(i)
+    let left = 0
+    let right = 0
+
+    while(left <= right && right < nums.length){
+
+        if(nums[left] % 2 === 1 && nums[right] % 2 === 0){
+            [nums[left],nums[right]]=[nums[right],nums[left]]
+            left+=1
+        }
+        
+        if(nums[left] % 2 !== 1) left+=1
+        if(nums[right] % 2 !==0) right+=1
     }
 
-    return [...even,...odd]
+
+    return nums
 };
