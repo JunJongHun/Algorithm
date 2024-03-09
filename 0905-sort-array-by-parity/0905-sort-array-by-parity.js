42bd59cd4caf97040683fb3d1aea4c8bd36064e6
Jonghoon <minn4072@gmail.com>
2024-03-10 08:39:55 +0900

    Time: 75 ms (18.91%), Space: 51.6 MB (66.24%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -5,19 +5,20 @@
 var sortArrayByParity = function(nums) {
 
     let left = 0
-    let right = 0
+    let right = nums.length-1
 
-    while(left <= right && right < nums.length){
-
-        if(nums[left] % 2 === 1 && nums[right] % 2 === 0){
-            [nums[left],nums[right]]=[nums[right],nums[left]]
-            left+=1
-        }
+    while(left<right){
         
-        if(nums[left] % 2 !== 1) left+=1
-        if(nums[right] % 2 !==0) right+=1
-    }
+        if(nums[left]%2===1 && nums[right]%2===0){
+            [nums[left],nums[right]] = [nums[right],nums[left]]
+            left++
+            right--
+            continue;
+       }
 
+       if(nums[left]%2 === 0) left++
+       if(nums[right]%2 === 1) right--
+    }
 
     return nums
 };
