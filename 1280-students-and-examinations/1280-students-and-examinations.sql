176744346c4f9551c0b8bd12a115187d478332b9
Jonghoon <minn4072@gmail.com>
2024-03-19 09:29:46 +0900

    Time: 1996 ms (28.03%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,6 +1,9 @@
-select s.student_id, s.student_name, b.subject_name,
-(select count(student_id) 
-from Examinations as e
-where s.student_id = e.student_id and b.subject_name = e.subject_name) as attended_exams
-from Students as s, Subjects as b
-order by s.student_id,b.subject_name
+select *,
+(
+    select count(*)
+    from Examinations as e
+    where a.student_id = e.student_id and b.subject_name = e.subject_name
+) as attended_exams
+from Students as a
+cross join Subjects as b
+order by a.student_id asc, b.subject_name asc
