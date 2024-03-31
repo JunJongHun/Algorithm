34b8dfb778c55913939db9b69db2e18ad69999f1
Jonghoon <minn4072@gmail.com>
2024-03-31 23:27:54 +0900

    Time: 914 ms (55.6%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,7 +1,2 @@
-select name
-from Customer
-where id not in (
-                select id
-                from Customer
-                where referee_id = 2
-)
+select name from customer
+where referee_id != 2 or referee_id is null;
